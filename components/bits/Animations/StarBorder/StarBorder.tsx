d5d230a3276193315d88727fcd3f41201fd999b2
Barr <147104390+Lyzer25@users.noreply.github.com>
2025-10-30 21:41:54 -0500

    fix starborder errors

--- components/bits/Animations/StarBorder/StarBorder.tsx
+++ components/bits/Animations/StarBorder/StarBorder.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from "react";
-import "./StarBorder.css";
+import styles from "./StarBorder.module.css";
 
 type StarBorderProps<T extends React.ElementType> =
   React.ComponentPropsWithoutRef<T> & {
@@ -30,39 +30,39 @@ const StarBorderInner = <T extends React.ElementType = "button">({
 
   return (
     <Component 
-      className={`star-border-container ${className}`} 
+      className={`${styles["star-border-container"]} ${className}`} 
       {...rest}
       style={componentStyle}
     >
       <div
-        className="border-gradient-bottom"
+        className={styles["border-gradient-bottom"]}
         style={{
           background: `radial-gradient(circle, ${color} 20%, transparent 80%)`,
           animationDuration: speed,
         }}
       ></div>
       <div
-        className="border-gradient-top"
+        className={styles["border-gradient-top"]}
         style={{
           background: `radial-gradient(circle, ${color} 20%, transparent 80%)`,
           animationDuration: speed,
         }}
       ></div>
       <div
-        className="border-gradient-left"
+        className={styles["border-gradient-left"]}
         style={{
           background: `radial-gradient(circle, ${color} 20%, transparent 80%)`,
           animationDuration: speed,
         }}
       ></div>
       <div
-        className="border-gradient-right"
+        className={styles["border-gradient-right"]}
         style={{
           background: `radial-gradient(circle, ${color} 20%, transparent 80%)`,
           animationDuration: speed,
         }}
       ></div>
-      <div className="inner-content">{children}</div>
+      <div className={styles["inner-content"]}>{children}</div>
     </Component>
   );
 };
